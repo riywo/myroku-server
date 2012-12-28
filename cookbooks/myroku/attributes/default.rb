bac6160e1b09aab7be0c2564c6640ef7bbed0b1a
Ryosuke IWANAGA <riywo.jp@gmail.com>
2012-12-27 16:45:52 -0800

    misc

--- cookbooks/myroku/attributes/default.rb
+++ cookbooks/myroku/attributes/default.rb
@@ -1,4 +1,7 @@
 default['myroku']['username'] = 'myroku'
 default['myroku']['ruby_version'] = '1.9.3-p327'
 
-default["redisio"]["default_settings"]["databases"] = 1000
+default['redisio']['default_settings']['databases'] = 1002
+
+default['authorization']['sudo']['groups'] = ['sudo']
+default['authorization']['sudo']['passwordless'] = true
--- cookbooks/myroku/recipes/proxy.rb
+++ cookbooks/myroku/recipes/proxy.rb
@@ -6,3 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+link "/etc/nginx/sites-enabled/myroku" do
+  to "/etc/nginx/sites-available/myroku"
+end
