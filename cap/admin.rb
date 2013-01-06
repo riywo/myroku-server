2c53cdb38fa73889af4b02c0399b1eb89a60e52b
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-01-06 13:51:12 +0000

    bug fix

--- cap/admin.rb
+++ cap/admin.rb
@@ -11,7 +11,7 @@ set :local_cache, "/var/myroku/cache/#{application}"
 set :deploy_to, "/var/myroku/#{application}"
 set :git_enable_submodules, true
 
-after "deploy", "env:deploy", "config:deploy", "cap:deploy", "triggerenv:deploy", "foreman:export"
+after "deploy", "env:deploy", "config:deploy", "triggerenv:deploy", "foreman:export"
 
 namespace :env do
   task :deploy do
@@ -24,15 +24,6 @@ namespace :config do
     run "rm -f #{deploy_to}/current/config/*.yml"
     Dir.glob("config/*.yml").each do |yml|
       upload yml, "#{deploy_to}/current/#{yml}"
-    end
-  end
-end
-
-namespace :cap do
-  task :deploy do
-    run "rm -f #{deploy_to}/current/cap/app/*.rb"
-    Dir.glob("cap/app/*.rb").each do |cap|
-      upload cap, "#{deploy_to}/current/#{cap}"
     end
   end
 end
